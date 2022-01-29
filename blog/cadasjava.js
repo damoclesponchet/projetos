c89856caddd3153742be5689ba91e0fbf20bd702
damoclesPonchet <97700857+damoclesponchet@users.noreply.github.com>
2022-01-29 11:15:41 -0300

    nov

---
+++ blog/cadasjava.js
@@ -0,0 +1,8 @@
+function criar(){
+    let nome = document.getElementById('nome')
+    let snome = document.getElementById('sobrenome')
+    let ema = document.getElementById('email')
+    let senha = document.getElementById('senha')
+    let csenha = document.getElementById('confirmarsenha')
+    window.location = "pprincipal.html" 
+}
