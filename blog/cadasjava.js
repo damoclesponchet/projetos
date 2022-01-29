e6723281079d8efba44349849cc543c2d9594837
damoclesPonchet <97700857+damoclesponchet@users.noreply.github.com>
2022-01-29 13:04:46 -0300

    n

--- blog/cadasjava.js
+++ blog/cadasjava.js
@@ -1,8 +1,12 @@
+let nome = document.getElementById('nome')
+let snome = document.getElementById('sobrenome')
+let ema = document.getElementById('email')
+let senha = document.getElementById('senha')
+let csenha = document.getElementById('confirmarsenha')
 function criar(){
-    let nome = document.getElementById('nome')
-    let snome = document.getElementById('sobrenome')
-    let ema = document.getElementById('email')
-    let senha = document.getElementById('senha')
-    let csenha = document.getElementById('confirmarsenha')
     window.location = "pprincipal.html" 
 }
+let a = document.getElementById('texto')
+let pa = document.createElement('p')
+pa.innerHTML = `esse é o seu nome ${nome} ${snome}`
+a.appendChild(pa)
